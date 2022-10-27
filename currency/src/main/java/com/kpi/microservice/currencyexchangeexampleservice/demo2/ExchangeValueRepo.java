a19bdfda6461b6c0a72e6c9f5dac3ed9f540ba7b
piyushbodhani1 <114053709+piyushbodhani1@users.noreply.github.com>
2022-10-27 18:00:09 +0530

    Delete currency directory

--- currency/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo2/ExchangeValueRepo.java
+++
@@ -1,7 +0,0 @@
-package com.kpi.microservice.currencyexchangeexampleservice.demo2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepo extends JpaRepository<ExchangeValueEntity,Integer>{
-ExchangeValueEntity findByFromAndTo(String from,String to);
-}
