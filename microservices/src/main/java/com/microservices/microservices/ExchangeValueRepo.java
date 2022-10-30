9d4d022d38fa31acf38a5e47520ec2d97b5c3809
piyushbodhani1 <114053709+piyushbodhani1@users.noreply.github.com>
2022-10-30 16:47:25 +0530

    Delete microservices directory

--- microservices/src/main/java/com/microservices/microservices/ExchangeValueRepo.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.microservices;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepo extends JpaRepository<ExchangeValueEntity, Integer> {
-	ExchangeValueEntity findByFromAndTo(String from, String to);
-}
