ea8193c712396aef476f3e9de2c5f8d2fcde385e
piyushbodhani1 <piyush.bodhani@kpipartners.com>
2022-10-31 18:50:10 +0530

    commit for "Test Cases"

--- EmpoyeeR/src/test/java/com/Employeee/EmpoyeeR/EmpoyeeRApplicationTests.java
+++ EmpoyeeR/src/test/java/com/Employeee/EmpoyeeR/EmpoyeeRApplicationTests.java
@@ -1,5 +1,12 @@
 package com.Employeee.EmpoyeeR;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +15,15 @@ class EmpoyeeRApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SampleController s = new SampleController();
+		List<String> list = s.getsample();
+		String ans  = list.get(0);
+		assertEquals(ans, "Abhitayu");
+		
+		
+		
+		 //assertEquals(Arrays.asList("Abhitayu","Madhav","om","Sagar", "Pravin"), s.getsample());
+		
 	}
 
 }
--- Schoollist/src/test/java/com/School/Schoollist/SchoollistApplicationTests.java
+++ Schoollist/src/test/java/com/School/Schoollist/SchoollistApplicationTests.java
@@ -1,13 +1,23 @@
 package com.School.Schoollist;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SchoollistApplicationTests {
+	@Autowired
+    private SchoolServices schoolService;
 
 	@Test
 	void contextLoads() {
+		List<SchoolEntity> school = schoolService.list();
+
+        assertEquals(school.size(), 5);
 	}
 
 }
